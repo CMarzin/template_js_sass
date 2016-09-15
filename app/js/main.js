96aa20b113644e0fbbc6e9e0d2f83610ca106b45
Corentin Marzin <corentinmarzin@gmail.com>
2016-09-15 14:50:48 +0200

    fix gulpfile + ajout fichier js export

--- app/js/main.js
+++ app/js/main.js
@@ -4,20 +4,10 @@ Loader.loadData('data/fr.json', function(data) {
   var t = test;
 
   function launch() {
-<<<<<<< HEAD
 
     var content_test = MyApp.templates.test(t);
 
     $('#test').html(content_test);
-
-=======
-    var t = test;
-    var test = MyApp.templates.test({
-        test: data.test
-    });
-    $('#test').html(test);
-    console.log('test');
->>>>>>> fa7337f5116cb74cc8d23857e7def937f25633b8
   }
 
   Anim.init();
