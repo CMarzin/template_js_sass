d43467dbdd51a4a74e692959168c69f760682dfb
Corentin Marzin <corentinmarzin@gmail.com>
2017-07-05 16:16:51 +0200

    [ADD] add boilerplate vuejs

---
+++ webpack_vue/src/components/Hello.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data() {
+    return {
+      msg: 'Hello World !',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  font-weight: normal;
+}
+</style>
