e70fa7a2d8f1d686fb3c7d4f9f2093534461740b
CMarzin <corentinmarzin@Gmail.com>
2018-05-29 15:59:54 +0200

    [FIX] fix server down when you the script fails

--- gulp-es6/app/scripts/main.js
+++ gulp-es6/app/scripts/main.js
@@ -1,6 +1,7 @@
 let test = {
-  'coucou': () => { console.log('coucou c\'est moi')} 
+  'coucou': () => {
+    console.log('coucou c\'est moi')
+  }
 }
-
 
 test.coucou()
--- gulp-es6/gulpfile.babel.js
+++ gulp-es6/gulpfile.babel.js
@@ -7,45 +7,53 @@ import source from 'vinyl-source-stream';
 import browserSync from 'browser-sync';
 import sass from 'gulp-sass';
 import autoprefixer from 'gulp-autoprefixer';
+import notifier from 'node-notifier'
 
-gulp.task("html", () => {
-    return gulp.src("./app/*.html")
-        .pipe(gulp.dest("./build"))
+gulp.task('html', () => {
+    return gulp.src('./app/*.html')
+        .pipe(gulp.dest('./build'))
         .pipe(browserSync.stream());
 });
 
-gulp.task("styles", () => {
-    return gulp.src("./app/stylesheets/*.scss")
+gulp.task('styles', () => {
+    return gulp.src('./app/stylesheets/*.scss')
         .pipe(sass.sync().on('error', sass.logError))
         .pipe(autoprefixer())
-        .pipe(gulp.dest("./build/css"))
+        .pipe(gulp.dest('./build/css'))
         .pipe(browserSync.stream());
 });
 
-gulp.task("scripts", () => {
+gulp.task('scripts', () => {
     return browserify({
-            entries: ["./app/scripts/main.js"]
+            entries: ['./app/scripts/main.js']
         })
         .transform(babelify.configure({
-            presets: ["es2015"]
+            presets: ['es2015']
         }))
         .bundle()
-        .pipe(source("bundle.js"))
-        .pipe(gulp.dest("./build/js"))
+        .on('error', (error) => {
+            console.log(error.stack)
+            notifier.notify({
+                'title': 'Compile Error',
+                'message': error.message
+              });
+        })
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest('./build/js'))
         .pipe(browserSync.stream());
 });
 
-gulp.task("startServer", () => {
+gulp.task('startServer', () => {
     browserSync.init({
-        server: "./build"
+        server: './build'
     });
 });
 
 gulp.task('watch', () => {
     gulp.watch('./app/*.html', ['html']);
     gulp.watch(['./app/scripts/*.js'], ['scripts']);
-    gulp.watch(['./app/sass/**/*.scss'], ['styles']);
+    gulp.watch(['./app/stylesheets/**/*.scss'], ['styles']);
 });
 
-gulp.task("build", ["html", "scripts", "styles"]);
-gulp.task("dev", ["startServer", "watch"]);
+gulp.task('build', ['html', 'scripts', 'styles']);
+gulp.task('dev', ['build', 'startServer', 'watch']);
